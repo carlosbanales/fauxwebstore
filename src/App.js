760ac84a062842819b2658fb0f8d8083d6a1faa9
carlosbanales <charliebananas42@gmail.com>
2022-03-26 10:28:49 -0700

    log data from fake store api to console

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import { BrowserRouter } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Pages from './components/Pages';
 
 function App() {
+  const [storeData, setStoreData] = useState();
+
+  async function fetchData() {
+    try {
+      const promise = await fetch('https://fakestoreapi.com/products');
+      const data = await promise.json();
+      console.log(data);
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+
   return (
     <BrowserRouter>
       <Pages />

13c8bb8d7ec653d3b9c21a62c98e77993aadc9b6
carlosbanales <charliebananas42@gmail.com>
2022-03-30 22:46:13 -0700

    made css changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import './App.css';
-import Navigation from './components/Navigation';
+import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState} from 'react';
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import ProductDetails from './components/ProductDetails';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import NotFound from './components/NotFound';
+import Home from './components/Home';
 
 function App() {
+  const [productList, setProductList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  async function fetchData() {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`https://fakestoreapi.com/products`);
+      const data = await res.json();
+      setProductList(data);
+      console.log('useEffect ran');
+      setIsLoading(false);
+    } catch(error) {
+      console.log(error);
+    }
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
   return (
-    <Navigation />
-  );
-}
+    <>
+      <Navbar />
+      <Routes>
+        <Route index element={ <Home /> } />
+        <Route
+          path="/products"
+          element={ <Products products={productList} loading={isLoading}/> }
+        />
+        <Route
+          path="/products/:id"
+          element={ <ProductDetails products={productList} /> }
+        /> 
+        <Route path="/cart" element={ <Cart /> } /> 
+        <Route path="/checkout" element={ <Checkout /> } />
+        <Route path="*" element={ <NotFound /> } /> 
+      </Routes>
+    </>
+  )
+};
 
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,23 @@
 import ProductCard from './ProductCard';
 import { Outlet } from 'react-router-dom';
 
-function Products({products}) {
+function Products({products, loading}) {
   return (
-    <>
-      <Outlet />
-      {products.map((item) => (
-        <ProductCard
-          key={item.id}
-          Item = {item}
-          Title={item.title}
-          Price={item.price}
-          Img={item.image}
-        />
-      ))}
-    </>
+      <>
+        { loading ? <div>...loading</div> :
+        <div className="products_container">
+          <Outlet />
+          {products.map((item) => (
+            <ProductCard
+              key={item.id}
+              Item = {item}
+              Title={item.title}
+              Price={item.price}
+              Img={item.image}
+            />
+          ))}
+        </div>}
+      </>
   )
 };
 
